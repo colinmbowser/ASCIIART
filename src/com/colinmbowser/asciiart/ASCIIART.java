0f0fcddb751715b982c5eeafc4dcf192906729ee
Colin Bowser <cbowser@byui.edu>
2022-10-06 20:50:13 -0700

    added images

--- src/com/colinmbowser/asciiart/ASCIIART.java
+++ src/com/colinmbowser/asciiart/ASCIIART.java
@@ -1,7 +1,27 @@
 package com.colinmbowser.asciiart;
 
-public class ASCIIART {
-    public static void main(String[] args) {
+import javax.swing.*;
+
+public class ASCIIART
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World People! new comment");
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new imagetests());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
     }
+
+
+
+
+
+
+
+
+
 }
